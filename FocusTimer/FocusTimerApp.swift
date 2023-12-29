de841e1a5ef357b22cc62e7a84a1be8097775e37
acifre <anthonybcifre@gmail.com>
2023-12-29 17:27:56 -0600

    Initial Commit

---
+++ FocusTimer/FocusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerApp.swift
+//  FocusTimer
+//
+//  Created by Anthony Cifre on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
