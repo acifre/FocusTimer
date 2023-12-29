1e70971496a9e5ae3055fdf3ef520617ca89b8fc
acifre <anthonybcifre@gmail.com>
2023-12-29 17:40:07 -0600

    First commit

---
+++ FocusTimer/PomodoroTimer.swift
@@ -0,0 +1,8 @@
+//
+//  PomodoroTimer.swift
+//  FocusTimer
+//
+//  Created by Anthony Cifre on 12/29/23.
+//
+
+import Foundation
---
+++ FocusTimer/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  FocusTimer
+//
+//  Created by Anthony Cifre on 12/29/23.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
